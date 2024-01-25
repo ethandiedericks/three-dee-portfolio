c736e79880a97805c7bfe3d050e656ace9b9b702
Ethan Diedericks <diedericksethan@gmail.com>
2024-01-25 23:21:13 +0200

    configured emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -40,9 +40,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Ethan Diedericks",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: import.meta.env.TO_EMAIL,
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
@@ -50,7 +50,7 @@ const Contact = () => {
       .then(
         () => {
           setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
+          alert("Thank you for contacting me! I will get back to you as soon as possible.");
 
           setForm({
             name: "",
