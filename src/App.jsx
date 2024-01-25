6f6e0f45464de30875cc0cfb05964f89f67fd890
Ethan Diedericks <diedericksethan@gmail.com>
2024-01-26 00:30:15 +0200

    configure vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Hero, Navbar, Tech, Projects, StarsCanvas } from "./components";
 
+
 const App = () => {
 
   return (
+    
    <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
